9364a3575481c931922c72a2b73c9ac351a8a9ea
Josh Leuze <jleuze@gmail.com>
2012-04-08 12:45:47 -0500

    Fixed nav in IE 6 & 7, added GIFs for IE6.

--- js/slideshow.js
+++ js/slideshow.js
@@ -7,35 +7,37 @@
 	http://www.jleuze.com/plugins/meteor-slides/customizing-the-slideshow-script/
 */
 
+// Set custom shortcut to avoid conflicts
 var $j = jQuery.noConflict();
 
-var $slidespeed = parseInt( meteorslidessettings.meteorslideshowspeed );
-
-var $slidetimeout = parseInt( meteorslidessettings.meteorslideshowduration );
-
-var $slideheight = parseInt( meteorslidessettings.meteorslideshowheight );
-
-var $slidewidth = parseInt( meteorslidessettings.meteorslideshowwidth );
-
+// Get the slideshow options
+var $slidespeed      = parseInt( meteorslidessettings.meteorslideshowspeed );
+var $slidetimeout    = parseInt( meteorslidessettings.meteorslideshowduration );
+var $slideheight     = parseInt( meteorslidessettings.meteorslideshowheight );
+var $slidewidth      = parseInt( meteorslidessettings.meteorslideshowwidth );
 var $slidetransition = meteorslidessettings.meteorslideshowtransition;
 
 $j(document).ready(function() {
+
+	// Setup jQuery Cycle
 	
     $j('.meteor-slides').cycle({
-		height: $slideheight,
-		width: $slidewidth,
-		fit: 1,
-		fx: $slidetransition,
-		speed: $slidespeed,
-		timeout: $slidetimeout,
-		pause: 1,
-		prev: '#meteor-prev',
-		next: '#meteor-next',
-		pager: '#meteor-buttons',
-		pagerEvent: 'click',
+		height:        $slideheight,
+		width:         $slidewidth,
+		fit:           1,
+		fx:            $slidetransition,
+		speed:         $slidespeed,
+		timeout:       $slidetimeout,
+		pause:         1,
+		prev:          '#meteor-prev',
+		next:          '#meteor-next',
+		pager:         '#meteor-buttons',
+		pagerEvent:    'click',
 		cleartypeNoBg: 'true',
-		slideExpr: '.mslide'
+		slideExpr:     '.mslide'
 	});
+	
+	// Setup jQuery TouchWipe
 
     $j('.meteor-slides').touchwipe({
         wipeLeft: function() {
@@ -46,10 +48,18 @@ $j(document).ready(function() {
         }
     });
 	
+	// Add class to hide and show prev/next nav on hover
+	
     $j('.meteor-slides').hover(function () {
 		$j(this).addClass('navhover');
     }, function () {
 		$j(this).removeClass('navhover');
     });
+	
+	// Set a fixed height for prev/next nav in IE6
+	
+	if(typeof document.body.style.maxWidth === 'undefined') {
+		$j('.meteor-nav a').height($slideheight);
+	}
 	
 });
